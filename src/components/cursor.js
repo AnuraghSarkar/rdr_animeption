403b68095040337d29fd4a179afaec3dd7b82329
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-03-17 10:55:07 +0545

    :clown_face: customized cursor component and video hover events

---
+++ src/components/cursor.js
@@ -0,0 +1,50 @@
+import React from "react";
+import gsap from "gsap";
+import { useGlobalStateContext } from "../context/globalContext";
+import tkv1 from "../images/tkv1.mp4";
+
+const Cursor = () => {
+  const { cursorType } = useGlobalStateContext();
+
+  const cursor = React.useRef(null);
+  const cursorConfigs = {
+    x: { previous: 0, current: 0, amt: 0.2 },
+    y: { previous: 0, current: 0, amt: 0.2 },
+  };
+
+  const onMouseMove = (event) => {
+    const { clientX, clientY } = event;
+    cursorConfigs.x = `${clientX}px`;
+    cursorConfigs.y = `${clientY}px`;
+
+    gsap.to(cursor.current, {
+      left: cursorConfigs.x,
+      top: cursorConfigs.y,
+      duration: 0.8,
+      ease: "Power3.easeOut",
+    });
+  };
+
+  React.useEffect(() => {
+    document.addEventListener("mousemove", onMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", onMouseMove);
+    };
+  }, []);
+
+  return (
+    <>
+      <div
+        className={`cursor ${!!cursorType ? "hovered" : ""} ${cursorType}`}
+        ref={cursor}
+      >
+        <div className="cursor__media">
+          <video src={tkv1} autoPlay loop muted></video>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Cursor;
